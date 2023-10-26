ca9af549de0f89793280e63c0533c29aaeeb0984
Thomas Bertok <thomasbertok@gmail.com>
2023-10-26 23:51:25 +0300

    delete with fire

--- src/config/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-    apiKey: "AIzaSyAJ7xmJ8nVXdt7B_ohqs70PaDeLQR11e0Q",
-    authDomain: "moviecards-vue.firebaseapp.com",
-    databaseURL: "https://moviecards-vue-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "moviecards-vue",
-    storageBucket: "moviecards-vue.appspot.com",
-    messagingSenderId: "1075692392698",
-    appId: "1:1075692392698:web:21d6b460517c63d467f943"
-};
--- src/config/omdb.js
+++
@@ -1,4 +0,0 @@
-export var OMDB_API = {
-    api_key: 'eb1327da',
-    api_url: 'http://www.omdb.com/'
-}
